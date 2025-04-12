c94af7f25f5ae8be8be9f4e7db4c787a119ff832
HudaOmer <hudaoe175@gmail.com>
2025-04-12 02:27:29 +0200

    Backtrack circular map

--- backtracking.py
+++ backtracking.py
@@ -1,25 +1,51 @@
 import random
 
 def Backtracking(map_csp):
-    for var in range(len(map_csp.variables)):
-        domain = map_csp.domain
+    var , iterations, back = 0 , 5, 0
+    domain = map_csp.domain
+    color = ["null"] + domain
+    allowed = list(range(1, len(domain) + 1))
+    allowed_history = [[] for _ in range(len(map_csp.variables))]
+    while var < len(map_csp.variables) and iterations:
         constrained = []
         for neighbor in map_csp.variables[var].neighbors:
             if neighbor.assigned_value > 0:
                 constrained.append(neighbor.assigned_value)
+        
+        
         allowed = list(set(range(1, len(domain) + 1)).symmetric_difference(set(constrained)))
+        allowed_history[var] = allowed
         if len(allowed):
             choice = random.choice(allowed)
-            print(choice)
             map_csp.variables[var].assigned_value = choice
+            var += 1
         else:
             print_line()
-            print(f"Failure: Conflict occures at state: {map_csp.variables[var].name}")
-            return -1
+            print(f"Backtracking from {map_csp.variables[var].name}")
+            print(map_csp.print_results())
+            map_csp.variables[var].assigned_value = 0
+            allowed_history[var] = []
+
+            var -= back
+            back += 1
+            if len(allowed_history[var]):
+                map_csp.variables[var - 1].assigned_value = random.choice(allowed_history[var])
+            
+            iterations -= 1
+            print(f"{var}")
+        print(f"{allowed_history=} & {allowed=}")
         
+            
+            
+    
     print_line()
-    print("Success! The final results are:")
+    if iterations:
+        print("Success! The final results are:")
+    else:
+        print("No solution exists!")
+        return -1
     return map_csp
+
 
 def print_line():
     print("----------------------------------------------")
--- variable.py
+++ variable.py
@@ -6,8 +6,6 @@ class Variable:
         self.assigned_value = assigned_value
 
     def __str__(self):
-        return f"{self.name}: {self.domain}, value: {self.assigned_value}"
+        return f"\n{self.name} -> Value: {self.assigned_value}, Domain: {self.domain} , Neighbors: {[self.neighbors[_].name for _ in range(len(self.neighbors))]}"
     def __repr__(self):
         return self.__str__()
-    def __eq__(self, other):
-        return self.name == other.name
